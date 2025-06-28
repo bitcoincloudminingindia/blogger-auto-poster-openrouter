9db9a65c3ed453901588e1b9ae54565c039afb1d
bitcoincloudmining <bitcoincloudminingindia@gmail.com>
2025-06-28 08:21:09 +0530

    Removed gemini_api.py since OpenRouter is used

--- gemini_api.py
+++
@@ -1,9 +0,0 @@
-
-import google.generativeai as genai
-
-genai.configure(api_key='AIzaSyCmXfBwITmVqJsi-1n0w2bAKOYn13Y0CYg')
-
-def generate_gemini_content(prompt):
-    model = genai.GenerativeModel('gemini-1.5-flash')
-    response = model.generate_content(prompt)
-    return response.text.strip()
